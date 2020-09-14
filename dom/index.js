7cb67ebdc2b1b02be71b45d156977672e6fb597c
Vladislav <mnebyvnebo1410@gmail.com>
2020-09-14 19:03:50 +0300

    Init commit

---
+++ dom/index.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+    'user strict';
+    
+    
+    paper.install(window);
+    paper.setup(document.getElementById('mainCanvas'));
+
+    var c = Shape.Circle(200, 200, 80);
+    c.fillColor = 'black';
+    var text = new PointText(200, 200);
+    text.justification = 'center';
+    text.fillColor = 'white';
+    text.fontSize = 20;
+    text.content = 'hello world';
+
+    paper.view.draw();
+})
